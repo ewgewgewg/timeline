3f9e5853734e6b0233c7dcdcbec67bb4a96edcd9
unknown <ed.w.greenberg@gmail.com>
2021-05-15 21:35:40 -0400

    group refactor

---
+++ src/assets/BritishIsles.js
@@ -0,0 +1,11 @@
+const BritishIslesPeriods = [
+  {
+    name: "Donald Trump",
+    flows: { "United States": true },
+    start: 2017,
+    end: 2021,
+    link: "https://en.wikipedia.org/wiki/Donald_Trump",
+  },
+];
+
+export default BritishIslesPeriods;
