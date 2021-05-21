ece961fb60edc6085e8b35e1154509ff54e80a77
unknown <ed.w.greenberg@gmail.com>
2021-05-20 23:56:21 -0400

    Valois

--- src/assets/France.js
+++ src/assets/France.js
@@ -1,4 +1,69 @@
 const FrancePeriods = [
+  //Valois
+  {
+    name: "Charles VII",
+    flows: { France: true, Valois: true },
+    start: 1422,
+    end: 1461,
+    link: "https://en.wikipedia.org/wiki/Charles_VII_of_France",
+  },
+  {
+    name: "Louis XI",
+    flows: { France: true, Valois: true },
+    start: 1461,
+    end: 1483,
+    link: "https://en.wikipedia.org/wiki/Louis_XI_of_France",
+  },
+  {
+    name: "Charles VIII",
+    flows: { France: true, Valois: true },
+    start: 1483,
+    end: 1498,
+    link: "https://en.wikipedia.org/wiki/Charles_VIII_of_France",
+  },
+  {
+    name: "Louis XII",
+    flows: { France: true, Valois: true },
+    start: 1498,
+    end: 1515,
+    link: "https://en.wikipedia.org/wiki/Louis_XII_of_France",
+  },
+  {
+    name: "Francis I",
+    flows: { France: true, Valois: true },
+    start: 1505,
+    end: 1547,
+    link: "https://en.wikipedia.org/wiki/Francis_I_of_France",
+  },
+  {
+    name: "Henry II",
+    flows: { France: true, Valois: true },
+    start: 1547,
+    end: 1559,
+    link: "https://en.wikipedia.org/wiki/Henry_II_of_France",
+  },
+  {
+    name: "Francis II",
+    flows: { France: true, Valois: true },
+    start: 1559,
+    end: 1560,
+    link: "https://en.wikipedia.org/wiki/Francis_II_of_France",
+  },
+  {
+    name: "Charles IX",
+    flows: { France: true, Valois: true },
+    start: 1560,
+    end: 1574,
+    link: "https://en.wikipedia.org/wiki/Charles_IX_of_France",
+  },
+  {
+    name: "Henry III",
+    flows: { France: true, Valois: true },
+    start: 1574,
+    end: 1589,
+    link: "https://en.wikipedia.org/wiki/Henry_III_of_France",
+  },
+  //Bourbon and Modern
   {
     name: "Henry IV",
     flows: { France: true, Bourbon: true },
